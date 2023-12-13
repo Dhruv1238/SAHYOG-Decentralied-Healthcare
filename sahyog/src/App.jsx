0647a4e47bdcf3024f6c932522045ab87128eb9f
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-12-13 22:02:34 +0530

    Added localStorage for Context

--- sahyog/src/App.jsx
+++ sahyog/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 
-
-
 function App() {
 
   return (
--- sahyog/src/pages/DetailsPage.jsx
+++ sahyog/src/pages/DetailsPage.jsx
@@ -5,11 +5,11 @@ import { Progress } from "@material-tailwind/react";
 import { Button } from '@material-tailwind/react';
 import { FaChevronRight } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
-
-
-const DetailButton = ({ text, onClick }) => {
+import { useFormContext } from '../context/FormProvider';
+const DetailButton = ({ text, onClick , color }) => {
+    const {form1 , updateFormData} = useFormContext();
     return (
-        <Button variant="outlined" color='white' className="flex items-center justify-between rounded-[20px] w-full gap-3 text-md font-bold bg-blue-500 font-inter" onClick={onClick}>
+        <Button variant="outlined" color='white' className={`flex items-center justify-between rounded-[20px] w-full gap-3 text-md font-bold ${!color ? "bg-blue-500" : "bg-green-500"} font-inter`} onClick={onClick}>
             {text}
             <FaChevronRight />
         </Button>
@@ -17,6 +17,7 @@ const DetailButton = ({ text, onClick }) => {
 }
 
 const DetailsPage = () => {
+    const { form1,form2,form3, updateFormData } = useFormContext();
     const navigate = useNavigate();
     return (
         <div className='flex flex-col gap-24 my-10 mx-7'>
@@ -29,9 +30,9 @@ const DetailsPage = () => {
                 <Typography color='white' className='text-3xl font-bold font-inter'>Hey There!!</Typography>
             </motion.div>
             <div className='flex flex-col gap-10'>
-                <DetailButton text='Basic Medical Details' onClick={() => navigate('/basicinfo')} />
-                <DetailButton text='Previous Diagnostic records' onClick={() => navigate('/medicaldetails')} />
-                <DetailButton text='Health Insurance Details' onClick={() => navigate('/healthinsurance')} />
+                <DetailButton color={form1.completed} text='Basic Medical Details' onClick={() => navigate('/basicinfo')} />
+                <DetailButton color={form2.completed} text='Previous Diagnostic records' onClick={() => navigate('/medicaldetails')} />
+                <DetailButton color={form3.completed} text='Health Insurance Details' onClick={() => navigate('/healthinsurance')} />
             </div>
         </div>
     )
