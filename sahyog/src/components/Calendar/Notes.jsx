036f9ede5afea410c2af615de9da4c5ede20f9f6
neilo <neil.carnac@somaiya.edu>
2023-12-14 02:54:12 +0530

    feat: added appbar.jsx and some more ui fixes in the Notes.jsx file

--- sahyog/src/components/Calendar/Notes.jsx
+++ sahyog/src/components/Calendar/Notes.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { CiShare1 } from "react-icons/ci";
+import { CiMenuKebab } from "react-icons/ci";
+import { IoIosArrowBack } from "react-icons/io";
+import App from '../../App';
 import Appbar from '../appbar/Appbar';
 
+
 const Notes = () => {
-  const { date, title, text, time } = useParams();
+  const { date , title , text , time } = useParams();
   const navigate = useNavigate();
 
   // const handleNavigateToCal = () => {
@@ -21,18 +26,20 @@ const Notes = () => {
     <>
     <div className="p-5 flex flex-row text-white">
       <div className="flex-1">
-      <button onClick={() => navigate(`/calendar`)} className='flex-1'>&lt; Calendar</button>
+      <button onClick={() => navigate(`/calendar`)} className='flex-1 flex font-extralight'><IoIosArrowBack className='text-cyan-300 flex-1 mt-1'/> 
+      <p className='flex-2 text-md font-normal text-cyan-300'>Calendar</p></button>
       </div>
       <div className="flex-2 flex flex-row gap-2 ">
-        <p className='flex-1'>S</p>
-        <p className='flex-2'>...</p>
+        <p className='flex-1 text-cyan-300 cursor-pointer'><CiShare1 /></p>
+        <p className='flex-2 text-cyan-300 cursor-pointer'><CiMenuKebab />
+ </p>
       </div>
     </div>
-      <div className="items-center p-5 pl-10 justify-center align">
+      <div className="items-center p-5 pl-8 justify-center align">
         <div className="flex flex-col items-start mt-2 text-white">
           <div className="flex flex-col flex-2">
-            <p className='flex-1 text-lg font-semibold text-cyan-300'>{date}</p>
-            <p className='text-gray-500 font-regular text-md flex-2'>9:43</p>
+          <p className='flex-1 text-lg font-semibold text-cyan-300'>{date}</p>
+          <p className='font-regular text-md text-gray-500 flex-2'>9:43</p>
           </div>
 
           <div className="mt-2 flex-3">
