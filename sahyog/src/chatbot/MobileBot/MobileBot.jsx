e8428bb557db8f26ba90a7225f2ec72c85a35dc7
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-12-14 04:55:42 +0530

    Added Mobile UI for ChatBot

---
+++ sahyog/src/chatbot/MobileBot/MobileBot.jsx
@@ -0,0 +1,44 @@
+import "./styles.css";
+import { SendBirdProvider, ChannelList, Channel } from "@sendbird/uikit-react";
+import "@sendbird/uikit-react/dist/index.css";
+import { useState } from "react";
+import ChatHeader from "./ChatHeader";
+import ChannelPreview from "./ChannelPreview";
+
+export default function MobileBot() {
+    const [channel, setChannel] = useState(null);
+
+    const onChannelSelect = (_channel) => {
+        setChannel(_channel);
+        window.history.pushState({}, _channel.name, "/" + _channel.url);
+    };
+
+    const onBack = () => {
+        setChannel(null);
+        window.history.pushState({}, document.title, "/");
+    };
+
+    return (
+        <div className="App">
+            <SendBirdProvider userId="1152" appId="25EBD513-9845-404D-9890-F22174F1630C">
+                {channel ? (
+                    <Channel
+                        channelUrl={channel.url}
+                        renderChatHeader={({ channel, user }) => (
+                            <ChatHeader channel={channel} user={user} onBack={onBack} />
+                        )}
+                    />
+                ) : (
+                    <ChannelList
+                        renderChannelPreview={({ channel }) => (
+                            <ChannelPreview
+                                channel={channel}
+                                onChannelSelect={onChannelSelect}
+                            />
+                        )}
+                    />
+                )}
+            </SendBirdProvider>
+        </div>
+    );
+}
