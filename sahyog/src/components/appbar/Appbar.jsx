4cd20e6d353e29f2904f04f23b62892e7b07efa9
Kunal Chaturvedi <kunalmanu2468@gmail.com>
2023-12-14 01:52:24 +0530

    feat: appbar component

---
+++ sahyog/src/components/appbar/Appbar.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import './Appbar.css';
+import { IoMdHome } from 'react-icons/io';
+import { FaCalendar } from 'react-icons/fa';
+import { Typography } from '@material-tailwind/react';
+import { FaUsers } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import appSos from '../../assets/appbar-sos.png';
+import { HiUsers } from "react-icons/hi";
+
+import { GoHomeFill } from 'react-icons/go';
+import { Link } from 'react-router-dom';
+
+const Appbar = () => {
+  const [selectedItem, setSelectedItem] = useState('home');
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const getItemStyles = (item) => {
+    return selectedItem === item
+      ? { color: '#002434' }
+      : { color: 'white' };
+  };
+
+  return (
+    <div className='flex w-screen h-[70px] bg-[#20ABE9] rounded-t-3xl justify-center items-center gap-2 absolute bottom-0'>
+    <div className='left flex gap-8  	'>
+        <Link to='/' >
+        <div
+          className='flex flex-col items-center home'
+          onClick={() => handleItemClick('home')}
+        >
+           {/* <GoHomeFill /> */}
+
+          <GoHomeFill
+            className='color-[#002434]'
+            style={getItemStyles('home')}
+            className='w-6 h-6'
+          />
+          <Typography
+            color='white'
+            variant='small'
+            style={getItemStyles('home')}
+          >
+            Home
+          </Typography>
+        </div>
+          </Link>
+        
+        <Link to='/calendar'>
+        <div
+          className='flex flex-col items-center home'
+          onClick={() => handleItemClick('calendar')}
+        >
+          <FaCalendar
+            color='white'
+            style={getItemStyles('calendar')}
+            className='w-6 h-6'
+
+          />
+          <Typography
+            color='white'
+            variant='small'
+            style={getItemStyles('calendar')}
+          >
+            Calendar
+          </Typography>
+        </div>
+          </Link>
+      </div>
+      <div className='sos-but mb-14 '>
+        <Link to='/sos'>
+        <img src={appSos} alt='Sos-button' />
+        </Link>
+      </div>
+      <div className='right flex gap-8'>
+          <Link to='/spaces'>
+        <div
+          className='flex flex-col items-center home'
+          onClick={() => handleItemClick('spaces')}
+        >
+          <HiUsers color='white' style={getItemStyles('spaces')}             className='w-6 h-6'
+/>
+          <Typography
+            color='white'
+            variant='small'
+            style={getItemStyles('spaces')}
+
+          >
+            Spaces
+          </Typography>
+        </div>
+            </Link>
+        <Link to='/profile'>
+
+        <div
+          className='flex flex-col items-center home'
+          onClick={() => handleItemClick('profile')}
+        >
+          <FaUser color='white' style={getItemStyles('profile')}   className='w-6 h-6'
+ />
+          <Typography
+            color='white'
+            variant='small'
+            style={getItemStyles('profile')}
+          >
+            Profile
+          </Typography>
+        </div>
+            </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Appbar;
