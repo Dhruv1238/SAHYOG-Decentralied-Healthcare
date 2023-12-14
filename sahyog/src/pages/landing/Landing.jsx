39a2143c67bde6d28cd028ab25e4c65d545c702d
Kunal Chaturvedi <kunalmanu2468@gmail.com>
2023-12-14 15:43:17 +0530

    feat: landing page refined

--- sahyog/src/pages/landing/Landing.jsx
+++ sahyog/src/pages/landing/Landing.jsx
@@ -10,7 +10,7 @@ import Appbar from '../../components/appbar/Appbar';
 import Navbar from '../../components/Navbar';
 import './Landing.css';
 import routinePhoto from "../../assets/routines.png"
-
+import ReactPlayer from 'react-player/youtube';
 
 const PersonCard = ({ photoSrc, name, degree, clickFunc }) => {
     return (
@@ -25,8 +25,65 @@ const PersonCard = ({ photoSrc, name, degree, clickFunc }) => {
         </div>
     );
 };
+// const YouTubeThumbnail = ({ videoUrl, thumbnailUrl }) => {
+//     const [showVideo, setShowVideo] = useState(false);
+  
+//     const playVideo = () => {
+//       setShowVideo(true);
+//     };
+  
+//     return (
+//       <div className="video-container" onClick={playVideo}>
+//         {!showVideo && <img src={thumbnailUrl} alt="Video Thumbnail" />}
+//         {showVideo && (
+//           <ReactPlayer
+//             url={videoUrl}
+//             width="100%"
+//             height="100%"
+//             controls
+//             playing
+//           />
+//         )}
+//       </div>
+//     );
+//   };
+
+const YouTubeThumbnail = ({ videoUrl }) => {
+    const [thumbnailUrl, setThumbnailUrl] = useState('');
+  
+    useEffect(() => {
+      const videoId = getVideoId(videoUrl);
+  
+      if (videoId) {
+        const generatedThumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+        setThumbnailUrl(generatedThumbnailUrl);
+      }
+    }, [videoUrl]);
+  
+    const getVideoId = (url) => {
+      const match = url.match(/[?&]v=([^&]+)/);
+      return match ? match[1] : null;
+    };
+  
+    return (
+      <div className="video-container">
+        {thumbnailUrl && (
+          <ReactPlayer
+            url={videoUrl}
+            width="100%"
+            height="100%"
+            controls
+          />
+        )}
+      </div>
+    );
+  };
+  
 
 const Landing = () => {
+
+    const videoUrl = 'https://www.youtube.com/watch?v=QFbupLSlPLE&pp=ygUNeW9nYSBhbmQgbGlmZQ%3D%3D';
+//   const thumbnailUrl = 'URL_TO_THUMBNAIL_IMAGE';
 
     // const navigate = useNavigate();
     const navigate = useNavigate();
@@ -79,27 +136,30 @@ const Landing = () => {
                         <PersonCard name='Dr. ABC' degree='MBBS' photoSrc={portrait} clickFunc={clickhandler} />
                     </div>
                 </div>
-                <div className='flex flex-col mb-32'>
+                <div className='flex flex-col gap-2'>
                     <div>
                         <Typography color='white' className='text-3xl font-bold font-inter'>Daily routines</Typography>
                     </div>
                     <div>
-                        <img src={routinePhoto} alt='banner' className='scale-' />
+                        <img src={routinePhoto} alt='banner' className='' />
                     </div>
                 </div>
-                <div className='flex flex-col mb-32'>
+                <div className='flex flex-col gap-4 mt-[-50px] mb-32'>
                     <div>
                         <Typography color='white' className='text-3xl font-bold font-inter'>Communities and Resources </Typography>
                     </div>
-                    <div>
+                    <div className='flex flex-col gap-6'>
                         {/* <img src={routinePhoto} alt='banner' className='scale-' /> */}
-                        <iframe
+                        {/* <iframe
                             width="358"
                             height="315"
                             src="https://www.youtube.com/watch?v=QFbupLSlPLE&pp=ygUNeW9nYSBhbmQgbGlmZQ%3D%3D"
                             frameborder="2"
                             allowfullscreen
-                        ></iframe>
+                        ></iframe> */}
+                              {/* <YouTubeThumbnail videoUrl={videoUrl} /> */}
+                              <iframe width="358" height="200" src="https://www.youtube.com/embed/QFbupLSlPLE?si=3IxYw1T2vLLsQqpG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                              <iframe width="358" height="200" src="https://www.youtube.com/embed/6ajmuRg2o3Q?si=voOqYUfOFt2PAB7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
