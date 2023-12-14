d4794bcad4d0eb193ba6488cf7aab733028602d9
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-12-14 13:46:31 +0530

    Added additional UI in Doctor Chat

--- sahyog/src/pages/Chat/Channel.jsx
+++ sahyog/src/pages/Chat/Channel.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../components/FirebaseSDK';
 import './Chat.css';
-// import { auth } from '../../components/FirebaseSDK';
-// import firebase from 'firebase';
 import SendMessage from './SendMessage';
 import { useRef } from 'react';
 import { collection, query, onSnapshot, orderBy, limit } from "firebase/firestore";
+import { Typography } from '@material-tailwind/react';
+
 const Channel = () => {
     const scroll = useRef()
     const [messages, setMessages] = useState([])
+
     useEffect(() => {
         const q = query(collection(db, "messages"), orderBy("createdAt"), limit(50));
         const unsubscribe = onSnapshot(q, (snapshot) => {
-            setMessages(snapshot.docs.map(doc => doc.data()));
+            setMessages(snapshot.docs.map(doc => doc.data())); // Reverse the order
         });
         return unsubscribe;
     }, []);
+
     return (
-        <div>
+        <div className='flex flex-col m-5'>
+            <Typography className='text-3xl text-white '>Chat with Shubham Patil</Typography>
             <div className="msgs">
-                {messages.map(({ id, text }) => (
-                    <div className=''>
-                        <div key={id} className='p-3 msg sent'>
-                            <p className='text-white'>{text}</p>
-                        </div>
+                {messages.map(({ id, text, sentBy }) => (
+                    <div key={id} className={`p-3 msg ${sentBy === 'Patient' ? 'sent' : 'received'}`}>
+                        <p className='text-white'>{text}</p>
                     </div>
                 ))}
             </div>
             <SendMessage scroll={scroll} />
-            <div ref={scroll}></div>
         </div>
-    )
+    );
 };
 
 export default Channel;
